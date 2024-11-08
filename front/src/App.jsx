99a7d4d16faec7d63600c352bbc5bd35c6bed636
reuby-dev <delphineb57@gmail.com>
2024-11-08 19:12:43 +0100

    add focus logical and switch cards

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,64 @@
 import './sass/main.scss';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [values, setValues] = useState({
+    name: '',
+    number: '',
+    expMonth: '',
+    expYear: '',
+    secretCode: ''
+  })
+
+  const handleChange = (event) => {
+    //get the name and the value of the target element
+    const { name, value } = event.target;
+
+    //keep all values, and change only element targeted
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+
+  const cardBack = useRef(null);
+  const cardFront = useRef(null);
+
+  const handleFocus = (event) => {
+    console.log(event.target)
+    const { dataset } = event.target
+
+    console.log('avant if', cardBack.current.classList)
+
+    if (dataset.cardPosition === "back" && cardBack.current.classList.contains("back-card")) {
+      cardBack.current.className = "back-card go-front"
+      cardFront.current.className = "front-card go-back"
+    } else {
+      cardBack.current.className = "back-card go-back"
+      cardFront.current.className = "front-card go-front"
+    }
+  }
+
+
   return (
     <>
       <main className="main-card-page">
         <div className="main-left">
           <div className="bank-card-container">
             <div className="bank-card">
-              <div className="front-card">
-                <p id="card-number">0000 0000 0000 0000</p>
+              <div className="front-card" ref={cardFront}>
+                <p id="card-number">{values.number}</p>
                 <div className="front-card-bottom">
-                  <p id="card-name">name</p>
-                  <p><span id="expiration-month">00</span> / <span id="expiration-year">00</span></p>
+                  <p id="card-name">{values.name}</p>
+                  <p>
+                    <span id="expiration-month">{values.expMonth}</span>
+                    /
+                    <span id="expiration-year">{values.expYear}</span></p>
                 </div>
               </div>
-              <div className="back-card">
-                <p id="secret-code">000</p>
+              <div className="back-card" ref={cardBack}>
+                <p id="secret-code">{values.secretCode}</p>
               </div>
             </div>
           </div>
@@ -25,29 +68,29 @@ function App() {
             <div className="form-lane">
               <label htmlFor="cardholder-name">Cardholder Name </label>
               <input type="text" name="name" id="cardholder-name" placeholder="e.g. Jane Appleseed"
-                data-text-replicate="card-name" data-card-position="front" required />
+                data-card-position="front" value={values.name} onChange={handleChange} onFocus={handleFocus} required />
             </div>
             <div className="form-lane">
               <label htmlFor="card-number-front">Card Number </label>
-              <input type="text" name="card-number-front" id="card-number-front" placeholder="e.g. 1234 5678 9123 0000"
-                maxLength="16" data-text-replicate="card-number" data-card-position="front" required />
+              <input type="text" name="number" id="card-number-front" placeholder="e.g. 1234 5678 9123 0000"
+                maxLength="16" data-card-position="front" value={values.number} onChange={handleChange} onFocus={handleFocus} required />
             </div>
             <div className="form-block">
               <fieldset className="form-lane small-lane">
                 <legend>Exp. Date (MM/YY)</legend>
                 <div className="double-lane">
                   <label htmlFor="expiration-month"></label>
-                  <input type="text" name="expiration-month" placeholder="MM" maxLength="2" id="expiration-month-input"
-                    data-text-replicate="expiration-month" data-card-position="front" required />
+                  <input type="text" name="expMonth" placeholder="MM" maxLength="2" id="expiration-month-input"
+                    data-card-position="front" value={values.expMonth} onChange={handleChange} onFocus={handleFocus} required />
                   <label htmlFor="expiration-year"></label>
-                  <input type="text" name="expiration-year" placeholder="YY" maxLength="2" id="expiration-year-input"
-                    data-text-replicate="expiration-year" data-card-position="front" required />
+                  <input type="text" name="expYear" placeholder="YY" maxLength="2" id="expiration-year-input"
+                    data-card-position="front" value={values.expYear} onChange={handleChange} onFocus={handleFocus} required />
                 </div>
               </fieldset>
               <div className="form-lane">
                 <label htmlFor="secret-code">CVC</label>
-                <input type="text" name="secret-code" placeholder="e.g. 123" id="secret-code-input"
-                  data-text-replicate="secret-code" maxLength="3" data-card-position="back" required />
+                <input type="text" name="secretCode" placeholder="e.g. 123" id="secret-code-input"
+                  maxLength="3" data-card-position="back" value={values.secretCode} onChange={handleChange} onFocus={handleFocus} required />
               </div>
             </div>
             <input type="submit" className="button" id="valid-form-button" value="Confirm" />
